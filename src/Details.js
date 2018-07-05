67e790f7c20baec2cfeb7de08e5fdb8de1c12a00
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-05 17:08:17 +0100

    intriduced state

--- src/Details.js
+++ src/Details.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Details extends Component {
-    render() {
-        return(
-              <div className='Details'>
-                <p>Hello, this will be the details page for each Movie & TV show :)</p>
-                <Link to="/"><button>Home</button></Link>
-              </div>              
-        );
-      }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "Hello, this will be the details page for each Movie & TV show :)",
     }
+  }
+
+  componentDidMount(){
+    setTimeout(() => this.setState({message:'Coming soon! :)'}) , 3000);
+  }
+
+  render() {
+    return (
+      <div className='Details'>
+      <h2>{this.state.message}</h2>
+        <Link to="/"><button>Home</button></Link>
+      </div>
+    );
+  }
+}
 
 export default Details;
