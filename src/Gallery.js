0f28a81b76b48e7b82d23a072e44f8a4ee555850
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-05 16:12:52 +0100

    completed task by 12 by seperating data structure and iterating over an array in stead of being hard coded within the app

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
 import Cover from './Cover.js';
-import blackMirror from './covers/blackMirror.jpg';
-import breakingBad from './covers/breakingBad.jpg';
-import deathNote from './covers/deathNote.jpg';
-import gameOfTHrones from './covers/gameofthrones.jpg';
-import theWalkingDead from './covers/thewalkingdead.jpg';
-import theWire from './covers/thewire.jpg';
+import tvShow from './GalleryInfo';
 
 class Gallery extends Component {
     render() {
         return (
-            <div>
-                <div className='container'>
-                    <div className='image-cover-row'>
-                        <Cover title="Black Mirror" image={blackMirror} id="Black-Mirror"/>
-                        <Cover title="Breaking Bad" image={breakingBad} id="Breaking-Bad" />
-                        <Cover title="Death Note" image={deathNote} id="Death-Note"/>
-                        <Cover title="Game of Thrones" image={gameOfTHrones} id="Game-of-thrones" />
-                        <Cover title="The Walking Dead" image={theWalkingDead} id="The-walking-dead" />
-                        <Cover title="The Wire" image={theWire} id="The-wire"/>
-                    </div>
-                </div>
+            <div className='container'>
+                {
+                    tvShow.map(show => <Cover key={show.id} id={show.id} image={show.image} title={show.title} />)
+                }
             </div>
         );
     }
---
+++ src/GalleryInfo.js
@@ -0,0 +1,44 @@
+import blackMirror from './covers/blackMirror.jpg';
+import breakingBad from './covers/breakingBad.jpg';
+import deathNote from './covers/deathNote.jpg';
+import gameOfTHrones from './covers/gameofthrones.jpg';
+import theWalkingDead from './covers/thewalkingdead.jpg';
+import theWire from './covers/thewire.jpg';
+
+let tvShow = [
+
+    {
+        title: 'black Mirror',
+        image: blackMirror,
+        id: 'Black-Mirror'
+    },
+    {
+        title: 'Breaking Bad',
+        image: breakingBad,
+        id: 'Breaking-Bad'
+    },
+    {
+        title: 'Death Note',
+        image: deathNote,
+        id: 'Death-Note'
+    },
+    {
+        title: 'Game of Thrones',
+        image: gameOfTHrones,
+        id: 'Game-of-thrones'
+    },
+    {
+        title: 'The Walking Dead',
+        image: theWalkingDead,
+        id: 'The-walking-dead'
+    },
+    {
+        title: 'The Wire',
+        image: theWire,
+        id: 'The-wire'
+    },
+
+];
+
+
+export default tvShow;
