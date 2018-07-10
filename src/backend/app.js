6d8b90a897f2c204a490c1acd78d30c10a8b542a
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-10 16:23:50 +0100

    added app use

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3001;
 
 app.get('/rest/shows', (req, res) => res.send(shows));
 
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
