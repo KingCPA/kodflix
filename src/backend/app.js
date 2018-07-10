72c4fca8a9fdadbbe393b0e643740ba175e9bde9
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-10 13:57:55 +0100

    completed task 22 regarding adding a backend call from server

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,5 +4,4 @@ let shows = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(shows));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -3,11 +3,31 @@ import Cover from './Cover.js';
 import tvShow from './GalleryInfo';
 
 class Gallery extends Component {
+    constructor() {
+        super();
+        this.state = {
+            TvShow: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/rest/shows')
+            .then(response => response.json())
+            .then(shows => console.log( shows) );
+
+        this.setState({TvShow: tvShow})
+    }
+
+
     render() {
+        let shows = this.state.TvShow
+        if (!shows) {
+            return <div>Loading...</div>
+          }
         return (
             <div className='container'>
                 {
-                    tvShow.map(show => <Cover key={show.id} id={show.id} image={show.image} title={show.title} />
+                    shows.map(show => <Cover key={show.id} id={show.id} image={show.image} title={show.title} />
                     )}
             </div>
         );
