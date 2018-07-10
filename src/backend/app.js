77c05f5b15f0ad13b9cee75698043d5c16ff5e92
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-10 16:13:50 +0100

    cancel

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 let shows = require('./shows');
+const port = process.env.PORT || 3001;
 
 app.get('/rest/shows', (req, res) => res.send(shows));
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(port, () => console.log('Example app listening on port 3001!'));
