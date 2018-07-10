13eae2f499207c180c9a5bd822aecbf4674ae06a
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-10 16:49:20 +0100

    added a variable for path

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3001;
 
 app.get('/rest/shows', (req, res) => res.send(shows));
 
+var path = require("path");
+
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
