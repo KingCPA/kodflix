8727fae4d6f750e64a0390c2b87a3edb73c3cd77
Aaron King <lbdrking2002@yahoo.co.uk>
2018-07-26 15:27:38 +0100

    Google Analytics update task 31

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -4,11 +4,24 @@ import Gallery from './Gallery';
 import Details from './Details';
 import BadUrl from './NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
+import ReactGA from 'react-ga';
 
 
 class App extends Component {
+  constructor({ history, location }) {
+    super();
+    ReactGA.initialize('UA-122882132-1')
+    this.trackViewPage(location)
+    history.listen((location) => this.trackViewPage(location))
+  }
+
+  trackViewPage(location) {
+    var host = window.location.hostname;
+    if(host !== "localhost") {
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+    }
+  };
+
   render() {
     return (
       <div className="App">
@@ -21,6 +34,9 @@ class App extends Component {
             </Switch>
           </div>
         </Router>
+        <div>
+          <ReactGA pageviews="" pathname="" search="" hash="" />
+        </div>
       </div>
     );
   }
